bfe375a89378b2656b17ed39c9c1088bd508ef87
Hudson Ayers <hayers@stanford.edu>
2020-12-01 14:51:33 -0800

    add time measurements, slight optimizations

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,14 +1,16 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::vec;
+extern crate elapsed;
+use elapsed::measure_time;
 
 fn main() {
     let filename = "input/input.txt";
     let input = file_to_vec(filename);
-    let ret = solve_part_1(&input);
-    let ret2 = solve_part_2(&input);
-    println!("part 1: {:?}", ret);
-    println!("part 2: {:?}", ret2);
+    let (time, ret) = measure_time(|| solve_part_1(&input));
+    let (time2, ret2) = measure_time(|| solve_part_2(&input));
+    println!("part 1: {} in {}", ret, time);
+    println!("part 2: {} in {}", ret2, time2);
 }
 
 fn file_to_vec(filename: &str) -> Vec<i64> {
@@ -23,24 +25,24 @@ fn file_to_vec(filename: &str) -> Vec<i64> {
 
 fn solve_part_1(numbers: &Vec<i64>) -> i64 {
     for (idx1, num1) in numbers.iter().enumerate() {
-        for num2 in numbers.iter().skip(idx1 + 1) {
+        for num2 in numbers[idx1 + 1..].iter() {
             if num1 + num2 == 2020 {
                 return num1 * num2;
             }
         }
     }
-    panic!("no solution");
+    panic!("no solution part 1");
 }
 
 fn solve_part_2(numbers: &Vec<i64>) -> i64 {
     for (idx1, num1) in numbers.iter().enumerate() {
-        for (idx2, num2) in numbers.iter().skip(idx1 + 1).enumerate() {
-            for num3 in numbers.iter().skip(idx1 + idx2 + 2) {
+        for (idx2, num2) in numbers[idx1 + 1..].iter().enumerate() {
+            for num3 in numbers[idx1 + idx2 + 2..].iter() {
                 if num1 + num2 + num3 == 2020 {
                     return num1 * num2 * num3;
                 }
             }
         }
     }
-    panic!("no solution");
+    panic!("no solution part 2");
 }
